1cbf3fc0da36fb583f04022319c86a62e053b8b3
ubuntu-work <solak.mert@hotmail.com>
2021-04-20 13:37:58 -0700

    feat(src/shared/configs/database.config.ts): database configuration file created

---
+++ src/shared/configs/database.config.ts
@@ -0,0 +1,26 @@
+import * as envLoader from 'load-env-var';
+
+import { SequelizeModuleOptions } from '@nestjs/sequelize';
+
+export default {
+  dialect: 'postgres',
+  host: envLoader.loadString('DB_HOST'),
+  port: envLoader.loadNumber('DB_PORT'),
+  logging: false,
+  native: false,
+  define: {
+    underscored: false,
+    createdAt: true,
+    updatedAt: true,
+  },
+  database: envLoader.loadString('DB_NAME'),
+  username: envLoader.loadString('DB_USERNAME'),
+  password: envLoader.loadString('DB_PASSWORD'),
+  models: ['modules/**/entities/*.{js,ts}'],
+  sync: {
+    force: false,
+    alter: true,
+  },
+  synchronize: true,
+  autoLoadModels: true,
+} as SequelizeModuleOptions;
